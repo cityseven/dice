6e79c0855b68f65558ff205f442e2efd8609ae29
cityseven <49065505+cityseven@users.noreply.github.com>
2019-04-02 15:47:12 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ require_once('../vendor/autoload.php');
 
 $router = new Router();
 
-$home = function (Router $router) {
+$router->allow('/', function (Router $router) {
     print '<p>Roll The Dice:
 	<a href="' . $router->getUriBase() . '/4">d4</a>
 	<a href="' . $router->getUriBase() . '/6">d6</a>
@@ -21,26 +21,25 @@ $home = function (Router $router) {
 	<a href="' . $router->getUriBase() . '/64">d64</a>
 	<a href="' . $router->getUriBase() . '/100">d100</a>
     </p>';
-};
-
-$roll = function (Router $router) {
-
-    $roll = random_int(1, (int) $router->getVars()[0]);
-    print $roll;
-};
-
-$router->allow('/', $home);
-$router->allow('/?', $roll);
+});
+$router->allow('/?', function (Router $router) {
+    $sides = $router->getVars()[0];
+	
+    if (preg_match('/^[0-9]+$/', $sides) !== 1) {
+    	die('Dice Not Found');
+    }
+	
+    $roll = random_int(1, (int) $sides);
+    print 'd' . $sides . ' roll is: ' . $roll;
+});
 
 $match = $router->match();
 if (!is_callable($match) ) {
-    notFound();
-}
-
-$match($router);
-
-
-function notFound()
-{
     die('Page Not Found');
 }
+
+?><html><head><title>Roll the dice</title></head><body><?php
+	
+$match($router);
+
+?></body></html>
