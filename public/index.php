6fd63925baf1076fa13b0df748f045def4b3d791
cityseven <49065505+cityseven@users.noreply.github.com>
2019-04-05 15:45:48 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,36 +10,26 @@ require_once('../vendor/autoload.php');
 $router = new Router();
 
 $router->allow('/', function (Router $router) {
-    print '<p>Roll The Dice:
-    	<a href="' . $router->getUriBase() . '/2">d2</a>
-	<a href="' . $router->getUriBase() . '/4">d4</a>
-	<a href="' . $router->getUriBase() . '/6">d6</a>
-	<a href="' . $router->getUriBase() . '/8">d8</a>
-	<a href="' . $router->getUriBase() . '/10">d10</a>
-	<a href="' . $router->getUriBase() . '/12">d12</a>
-	<a href="' . $router->getUriBase() . '/20">d20</a>
-	<a href="' . $router->getUriBase() . '/32">d32</a>
-	<a href="' . $router->getUriBase() . '/64">d64</a>
-	<a href="' . $router->getUriBase() . '/100">d100</a>
-    </p>
-    <pre>
-                    ______
-        .-------.  /\     \
-       /   o   /| /o \  o  \
-      /_______/o|/   o\_____\
-      | o     | |\o   /o    /
-      |   o   |o/ \ o/  o  /
-      |     o |/   \/____o/
-      '-------'    
-    </pre>';
+    print '
+<p>Roll The Dice:<p>
+<p>
+<a href="' . $router->getUriBase() . '/2"><img src="img/d2.svg" alt="d2"></a>
+<a href="' . $router->getUriBase() . '/4"><img src="img/d4.svg" alt="d4"></a>
+<a href="' . $router->getUriBase() . '/6"><img src="img/d6.svg" alt="d6"></a>
+<a href="' . $router->getUriBase() . '/8"><img src="img/d8.svg" alt="d8"></a>
+<a href="' . $router->getUriBase() . '/10"><img src="img/d10.svg" alt="d10"></a>
+<a href="' . $router->getUriBase() . '/12"><img src="img/d12.svg" alt="d12"></a>
+<a href="' . $router->getUriBase() . '/20"><img src="img/d20.svg" alt="d20"></a>
+<a href="' . $router->getUriBase() . '/100"><img src="img/d100.svg" alt="d100"></a>
+</p>';
 });
 $router->allow('/?', function (Router $router) {
     $sides = $router->getVars()[0];
-	
+
     if (preg_match('/^[0-9]+$/', $sides) !== 1) {
     	die('Dice Not Found');
     }
-	
+
     $roll = random_int(1, (int) $sides);
     print 'd' . $sides . ' roll is: ' . $roll;
 });
@@ -50,7 +40,7 @@ if (!is_callable($match) ) {
 }
 
 ?><html><head><title>Roll the dice</title></head><body><?php
-	
+
 $match($router);
 
 ?></body></html>
