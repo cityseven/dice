bfa5826f51491330e5e887d5316ff695904b9014
cityseven <49065505+cityseven@users.noreply.github.com>
2019-04-10 12:30:44 +0200

    Update and rename d6.svg to d6.php

--- public/img/d6.php
+++ public/img/d6.php
@@ -1,4 +1,14 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<?php
+
+$sides = 6;
+$rollTheDice = 'd' . $sides;
+if (!isset($_GET['start'])) {
+    $rollTheDice = random_int(1, $sides);
+}
+
+header('Content-type: image/svg+xml');
+
+echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!-- Created with Inkscape (http://www.inkscape.org/) -->
 <svg
    xmlns:dc="http://purl.org/dc/elements/1.1/"
@@ -129,6 +139,7 @@
          y="108.226"
          x="88.652657"
          id="tspan3287"
-         sodipodi:role="line">d6</tspan></text>
+         sodipodi:role="line">' . $rollTheDice . '</tspan></text>
   </g>
 </svg>
+';
