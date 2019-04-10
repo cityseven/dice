047cc10e2ed66cb538598bffd7ee41f6544c2c26
cityseven <49065505+cityseven@users.noreply.github.com>
2019-04-10 12:31:28 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -7,31 +7,52 @@ use Attogram\Router\Router;
 
 require_once('../vendor/autoload.php');
 
+$diceSet = [2, 4, 6, 8, 10, 12, 20, 100];
+
 $router = new Router();
 
 $router->allow('/', function (Router $router) {
-    print '
-<p>Roll The Dice:<p>
-<p>
-<a href="' . $router->getHome() . '2"><img src="img/d2.svg" alt="d2"></a>
-<a href="' . $router->getHome() . '4"><img src="img/d4.svg" alt="d4"></a>
-<a href="' . $router->getHome() . '6"><img src="img/d6.svg" alt="d6"></a>
-<a href="' . $router->getHome() . '8"><img src="img/d8.svg" alt="d8"></a>
-<a href="' . $router->getHome() . '10"><img src="img/d10.svg" alt="d10"></a>
-<a href="' . $router->getHome() . '12"><img src="img/d12.svg" alt="d12"></a>
-<a href="' . $router->getHome() . '20"><img src="img/d20.svg" alt="d20"></a>
-<a href="' . $router->getHome() . '100"><img src="img/d100.svg" alt="d100"></a>
-</p>';
+    global $diceSet;
+
+    echo '<h1>Roll The Dice</h1><div style="display:flex;flex-flow:row wrap;">';
+    foreach ($diceSet as $dice) {
+        echo '<a href="' . $router->getHome() . $dice . '">
+<div style="border:1px solid #d0d0d0;text-align:center;">
+    <div>d' . $dice . '</div>
+    <div><img src="img/d' . $dice . '.php" alt="d' . $dice . '"></div>
+</div>
+</a>';
+    }
+    echo '</div>';
 });
+
+
 $router->allow('/?', function (Router $router) {
+    global $diceSet;
+
     $sides = $router->getVar(0);
 
     if (preg_match('/^[0-9]+$/', $sides) !== 1) {
-    	die('Dice Not Found');
+        die('Dice Not Found');
     }
 
-    $roll = random_int(1, (int) $sides);
-    print 'd' . $sides . ' roll is: ' . $roll;
+    print '<p><a href="' . $router->getHome() . '">Roll The Dice</a></p>';
+
+    print '<p>Rolling d' . $sides . '</p>';
+    if (in_array($sides, $diceSet)) {
+        print '<img src="img/d' . $sides . '.php">';
+    } else {
+        echo '<p>Roll d' . $sides . ' = <b>' . random_int(1, (int) $sides) . '</b></p>';
+    }
+});
+
+$router->allow('/?/d.svg', function (Router $router) {
+    global $diceSet;
+
+    $sides = $router->getVar(0);
+    if (!in_array($sides, $diceSet)) {
+        die('Dice Not Found');
+    }
 });
 
 $match = $router->match();
